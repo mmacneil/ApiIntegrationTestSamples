8a6f43d49264a79fc0952581938cfeb19e9de013
mmacneil <markmacneil@gmail.com>
2018-11-13 20:31:21 -0400

    Post wip.

--- src/Web.Api/Core/Domain/Entities/Player.cs
+++ src/Web.Api/Core/Domain/Entities/Player.cs
@@ -1,4 +1,5 @@
-﻿using Web.Api.Core.Shared;
+﻿using System;
+using Web.Api.Core.Shared;
 
 namespace Web.Api.Core.Domain.Entities
 {
@@ -7,16 +8,18 @@ namespace Web.Api.Core.Domain.Entities
         public string FirstName { get; private set; } // EF migrations require at least private setter - won't work on auto-property
         public string LastName { get; private set; }
         public int HeightCm { get; private set; }
-        public decimal WeightLbs { get; private set; }
+        public decimal WeightKg { get; private set; }
+        public DateTime BirthDate { get; private set; }
 
         internal Player() { /* Required by EF */ }
 
-        internal Player(string firstName, string lastName, int heightCm, decimal weightLbs)
+        internal Player(string firstName, string lastName, int heightCm, decimal weightKg,DateTime birthDate)
         {
             FirstName = firstName;
             LastName = lastName;
             HeightCm = heightCm;
-            WeightLbs = weightLbs;
+            WeightKg = weightKg;
+            BirthDate = birthDate;
         }
     }
 }
---
+++ src/Web.Api/Migrations/20181114001359_initial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    HeightCm = table.Column<int>(nullable: false),
+                    WeightKg = table.Column<decimal>(nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 1, new DateTime(1987, 8, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sidney", 180, "Crosby", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 91m });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 2, new DateTime(1997, 1, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Connor", 185, "McDavid", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 88m });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 3, new DateTime(1997, 9, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Auston", 191, "Matthews", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 100m });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 4, new DateTime(1988, 11, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Patrick", 180, "Kane", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 177m });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 5, new DateTime(1989, 12, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Drew", 185, "Doughty", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 200m });
+
+            migrationBuilder.InsertData(
+                table: "Players",
+                columns: new[] { "Id", "BirthDate", "Created", "FirstName", "HeightCm", "LastName", "Modified", "WeightKg" },
+                values: new object[] { 6, new DateTime(1990, 5, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Erik", 183, "Karlsson", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 190m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
